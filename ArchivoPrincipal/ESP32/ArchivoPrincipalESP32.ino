d7e2d8c43156eeb58bfa17f40e2f1779fb237dc9
Víctor <0619469228@uma.es>
2022-02-14 23:55:24 +0100

    ESP32 + Lectura + DeepSleep +RTC memory

--- ArchivoPrincipal/ESP32/ArchivoPrincipalESP32.ino
+++ ArchivoPrincipal/ESP32/ArchivoPrincipalESP32.ino
@@ -1,22 +1,53 @@
-// Potenciometro conectado a Gpio 35
-const int portPin1 = 35;
-const int portPin2 = 36;
-// Almacenamiento del valor de puerto (rango de 0-4095)
-double porValor1 = 0;
-double porValor2 = 0;
-void setup() {
-  Serial.begin(9600);
+    /////////////////////////////////////////////////////////////////
+   //       ESP32 Deep Sleep Y memoria TTC Prueba v1.00           //
+  //                 Víctor Coto Rodríguez                       //
+ /////////////////////////////////////////////////////////////////
+
+#define uS_TO_S_FACTOR 1000000  /* Conversion de microsegundos a segundos */
+#define TIME_TO_SLEEP  10        /* Tiempo en segundos que el microcontrolador va a dormir */
+#include <HX711.h>
+RTC_DATA_ATTR int Contador = 0; /* Utilizamos la memoria TTR para guardar el número despues del sueño*/
+
+const byte PIN_DATOS = 2;  //amarillo (señal a tratar)
+const byte PIN_RELOJ = 27; //verde
+int Pin_Azul = 25;         // Azul
+int Pin_Rojo = 26;         // Rojo
+//========================================================================
+//                         SETUP
+//========================================================================
+
+void setup(){
+gpio_hold_dis(GPIO_NUM_27);// desactiva el freezing despues del sueño profundo
+Serial.begin(9600);
+  pinMode(Pin_Azul,OUTPUT);
+  pinMode(Pin_Rojo,OUTPUT);
+  pinMode(PIN_RELOJ, OUTPUT);
   delay(500);
+  //////////////////////////////////////////////////////
 
+  if(Contador == 0) //Solo se ejecuta la primera vez
+  {
+      digitalWrite(Pin_Rojo,HIGH);
+      Contador = Contador+1;
+  }else
+  {
+      digitalWrite(Pin_Azul,HIGH);
+      Contador = Contador+1;
+  }
+  delay(10000);
+  digitalWrite(Pin_Azul,LOW);
+  digitalWrite(Pin_Rojo,LOW);
+  digitalWrite(PIN_RELOJ,HIGH);
+  
+  Serial.println(" ");
+  Serial.println("Numerode ciclos: ");
+  Serial.println(Contador);
+  gpio_hold_en(GPIO_NUM_27);
+  gpio_deep_sleep_hold_en();
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  esp_deep_sleep_start();
 }
-
-void loop() {
-// Lectura del valor en cada vuelta de bucle
-porValor1 = analogRead(portPin1);
-porValor2 = analogRead(portPin2);
-Serial.print("Este es el valor 1: ");
-Serial.println(porValor1 ); // Envio del valor al puerto serie
-Serial.print("Este es el valor 2: ");
-Serial.println(porValor2 ); // Envio del valor al puerto serie
-delay(1000);
+void loop(){
+  
 }
