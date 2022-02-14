290132d4060e3549de5124ef779aee14ab035a0e
Víctor <0619469228@uma.es>
2022-02-14 23:25:20 +0100

    se ha modificado para enviar cada medición para una demostración en vivo

--- ArchivoPrincipal/ArchivoPrincipalESP8266.ino
+++ ArchivoPrincipal/ArchivoPrincipalESP8266.ino
@@ -16,14 +16,12 @@ const byte PIN_RELOJ = 4;    //D2 //verde
  
 HX711 hx711(PIN_DATOS,PIN_RELOJ);
 
- 
-int rbutton = D4; // this button will be used to reset the scale to 0.
 float valor;
 float kilos;
 float anterior = 0;
-float calibration_factor = 1030853; // for me this vlaue works just perfect more or less
-float zero_factor = 154619;
-//float peso_conocido = 658.0;
+float calibration_factor = 947753; // for me this vlaue works just perfect more or less
+float zero_factor = 242614; //164019
+//float peso_conocido = 640.0;
 //=============================================================================================
 //                         SETUP
 //=============================================================================================
@@ -32,8 +30,6 @@ void setup()
 {
 
   pinMode(PIN_VCCHX711, OUTPUT);
-  pinMode(rbutton, INPUT_PULLUP); 
-
 
   Serial.begin(9600);  //preparar el puerto serie
   digitalWrite(PIN_VCCHX711, HIGH);
@@ -58,39 +54,25 @@ void setup()
   kilos = hx711.get_units(5); 
   valor = kilos*1000;
 
-  if(valor<0)
-  {
+  if(valor<=5){
+    if(valor<=-15){
+      ESP.deepSleep(2e6);
+    }
   valor = 0;
   }
-  Serial.print("Valor: ");
-  Serial.print(kilos);
-  Serial.println(" KG");
-  Serial.println();
-
-  Serial.print("Valor: ");
-  Serial.print(valor);
-  Serial.println(" g");
-  Serial.println();
-  
-  if ( digitalRead(rbutton) == LOW)
-{
-  hx711.set_scale();
-  hx711.tare(); //Reset the scale to 0
-}
 
 //if((anterior-valor)<=-25 ||(anterior-valor)>=25){
   ThingSpeak.setField (1,valor);
   ThingSpeak.writeFields(channelID,WriteAPIKey);
   Serial.println("Datos enviados a ThingSpeak");
   anterior = valor;
-  delay(14000);
+  //delay(14000);
 //}
+
 
 ESP.deepSleep(15e6);
 
 }
- 
-void loop() 
-{
+void loop() {
  
 }
