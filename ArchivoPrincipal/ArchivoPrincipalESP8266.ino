8eb7ee833964e7084637b406d6954c724ac1c33a
Víctor <0619469228@uma.es>
2022-02-14 22:40:50 +0100

    ESP8266 + Sensor + DeepSleep + Servidor
    En este archivo se ha implementado la toma de datos del sensor, posteriormente se ha añadido periodos de sueño profundo para reducir el consumo energético para finalmente enviar la información a un servidor de Thingspeak en la nube

--- ArchivoPrincipal/ArchivoPrincipalESP8266.ino
+++ ArchivoPrincipal/ArchivoPrincipalESP8266.ino
@@ -1,3 +1,7 @@
+
+
+
+
 #include <HX711.h>
 #include <ESP8266WiFi.h>
 #include <ThingSpeak.h>
@@ -14,13 +18,11 @@ const byte PIN_DATOS = 5;  //D1 //amarillo
 const byte PIN_RELOJ = 4;  //D2 //verde
  
 HX711 hx711(PIN_DATOS,PIN_RELOJ);
-
- 
-int rbutton = D4;
 float valor;
 float kilos;
 float anterior = 0;
 float calibration_factor = 1030853; 
+float zero_factor = 154619;
 //float peso_conocido = 658.0;
 //=============================================================================================
 //                         SETUP
@@ -28,11 +30,19 @@ float calibration_factor = 1030853;
 
 void setup() 
 {
+
+
+
+
+
   Serial.begin(9600);  //preparar el puerto serie
-  pinMode(rbutton, INPUT_PULLUP); 
-  hx711.set_scale();
-  hx711.tare(); //Resetea la escala a 0
-  long zero_factor = hx711.read_average(); //hace una lectura
+
+  long zero_factorx = hx711.read_average(); //Get a baseline reading
+  hx711.set_offset(zero_factor);
+  Serial.print("zero_factorx: ");
+  Serial.println(zero_factorx);
+  hx711.set_scale(calibration_factor); //Adjust to this calibration factor
+
 
  WiFi.begin(ssid, password);
 
@@ -43,14 +53,6 @@ void setup()
  Serial.println("Conectado a la red WIFI");
  ThingSpeak.begin(cliente);
 
-
-}
- 
-void loop() 
- 
-{
- 
-  hx711.set_scale(calibration_factor); //Adusta el factor de calibracion
  
   kilos = hx711.get_units(5); 
   valor = kilos*1000;
@@ -69,21 +71,20 @@ void loop()
   Serial.println(" g");
   Serial.println();
   
-  if ( digitalRead(rbutton) == LOW)
-{
-  hx711.set_scale();
-  hx711.tare(); //Resetea la escala a 0
-}
-
-
-if((anterior-valor)<=-25 ||(anterior-valor)>=25){
+//if((anterior-valor)<=-25 ||(anterior-valor)>=25){
   ThingSpeak.setField (1,valor);
   ThingSpeak.writeFields(channelID,WriteAPIKey);
   Serial.println("Datos enviados a ThingSpeak");
   anterior = valor;
   delay(14000);
+//}
+
+
+ESP.deepSleep(15e6);
+
 }
-
-
-
+ 
+void loop() 
+{
+ 
 }
